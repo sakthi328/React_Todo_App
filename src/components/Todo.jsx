77280c36f99601887ae8d60fec12943e896af885
sakthi328 <sakthisankar328@gmail.com>
2025-06-10 19:08:24 +0530

    API Integration with filters

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,29 +1,97 @@
-import  { useState } from "react";
+import  { useState,useEffect } from "react";
 import { TodoList } from "./Todolist";
 import AddIcon from '@mui/icons-material/Add';
+import { FormControl,Select,MenuItem } from "@mui/material";
+import { Task } from "@mui/icons-material";
 function Todo(){
     const [todoList, setTodoList] = useState([]);
     const [task, setTask] = useState("");
+    const [filter, setFilter] = useState("all");
+
+
+    const fetchTodos = async () => {
+  try {
+    const response = await fetch("https://684235efe1347494c31c255d.mockapi.io/todos");
+    const data = await response.json();
+    setTodoList(data);
+  } catch (error) {
+    console.error("Failed to fetch todos:", error);
+  }
+};
+useEffect(() => {
+  fetchTodos();
+}, []);
+const handleChange = (event) => {
+    setFilter(event.target.value);
+  };
     const handleAddTask = () =>{
       console.log(todoList)
      if(task == "") {
    alert("Task should not be empty")
-     }else{
-        setTodoList((prev) => [...prev, task]);
-        setTask("");
-console.log(todoList)
-     }  
-    } 
+     }
+     const newtask = {
+    task: task,
+    complete: false
+  };
+      try {
+                const payload = JSON.stringify(newtask)
+                fetch("https://684235efe1347494c31c255d.mockapi.io/todos",{method:"POST", headers:{'content-type':'application/json'},body:payload})
+                .then((response)=>response.json())
+                .then((data)=>{console.log(data);
+                
+            setTask("");     
+    fetchTodos();
+   }) .catch(error=>console.log(error))     
+  } catch (error) {
+    console.error("Failed to add task:", error);
+  }
+};
+// useEffect(() => {
+//   fetchTodos();
+// }, [task]);
+     
+     
+//      {
+//         setTodoList((prev) => [...prev, task]);
+//         setTask("");
+// console.log(todoList)
+//      }  
+    const filteredTodos = todoList.filter((item) => {
+  if (filter === "all") return true;
+  if (filter === "active") return item.complete === false;
+  if (filter === "complete") return item.complete === true;
+});
+
+    
  return(
    <div className="flex justify-center items-center mt-20 " >
-    <div className="bg-white  pb-6 pt-6 px-4 rounded shadow-md" style={{ height: "450px", width:"400px" }}>     
+    <div>     
         <p className=" text-3xl text-center rela font-mono mb-4 font-extrabold">TODO LIST</p>
-       <div className="relative"> <input className=" bg-gray-300 border border-gray-300 focus:outline-none pl-3 pr-12 rounded" value={task} name="todo-list" style={ {height :"40px", width:"340px"}} onChange={(e) => setTask(e.target.value)} />
-        <button className=" absolute right-6" style={{backgroundColor: "green",color: "white",height: "40px", width: "55px",alignItems:"center"}} onClick={handleAddTask}><AddIcon/></button></div>
-         <div>{todoList.map((item,index) => (
-          <TodoList key={index} index={index} item={item} setTodoList={setTodoList} />
-        ))}
+       <div > <input className=" bg-white text-2xl border  h-16 border-gray-300 focus:outline-none pl-3 pr-12 rounded  mb-8" value={task} name="todo-list" style={ {height :"58px", width:"550px"}} onChange={(e) => setTask(e.target.value)} />
+        <button className="mb-1"  style={{backgroundColor: "green",color: "white",height: "57px", width: "60px",alignItems:"center"}} onClick={handleAddTask}><AddIcon/></button>
+        
+
+            
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <Select sx={{ backgroundColor: 'white' }}
+          value={filter}
+          onChange={handleChange}
+          
+        >
+          
+          <MenuItem value={"all"}>All</MenuItem>
+          <MenuItem value={"active"}>Active</MenuItem>
+          <MenuItem value={"complete"}>Complete</MenuItem>
+        </Select>
+        
+      </FormControl>
+      
+        
         </div>
+         
+         <div>{filteredTodos.map((item, index) => (
+  <TodoList key={item.id} index={index} id={item.id} item={item} fetchTodos={fetchTodos} />
+))}</div>
     </div>
     </div>
  )   
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,37 +1,84 @@
 import React, { useState } from "react";
-import EditIcon from '@mui/icons-material/Edit';
+// import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check';
+import CancelIcon from '@mui/icons-material/Cancel';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-export const TodoList = (props) => { 
-    const { item, index, setTodoList } = props;
-    const [isEdit, setIsEdit] = useState(false);
-    const container_style = {
-        width: "345px",
-        padding: "5px 10px",
-        textAlign: "start",
-        display: "flex",
-        justifyContent: "space-between",
-        backgroundColor:"rgb(209,213,219)",
-        marginTop:"10px",
-        marginBottom:"10px"
+export const TodoList = ({ item, id, fetchTodos }) => { 
+    
+    
+    // const container_style = {
+    //     width: "345px",
+    //     padding: "5px 10px",
+    //     textAlign: "start",
+    //     display: "flex",
+    //     justifyContent: "space-between",
+    //     backgroundColor:"white",
+    //     marginTop:"10px",
+    //     marginBottom:"10px"
     
     
         
-    };
+    // };
 
-    const handleEditChange = (e) => {
-        const { value } = e.target;
-        setTodoList(prev => (prev.map((list, Index) => index === Index ? value : list)));
-    };
-    const handleDelete = () =>{
-        setTodoList(prev=>(prev.filter((list,Index)=>Index !==index)))
+    
+
+
+const handleToggleComplete = () => {
+try {
+      const paylod = JSON.stringify({complete: !item.complete});
+fetch(`https://684235efe1347494c31c255d.mockapi.io/todos/${id}`, {
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: paylod,
+      })
+      .then((response) => response.json())
+        .then(() => {
+          fetchTodos();
+        }) .catch((error) => console.log(error));
+    }  catch (error) {
+      console.log(error);
     }
-    return(
-        <div style={container_style}>
-            {isEdit ? <input className=" bg-[rgb(209,213,219)] focus:outline-none " type="text" onChange={handleEditChange} value={item} /> : <p>{item}</p>}
-            <div>
-                <button  onClick={() => setIsEdit(!isEdit)}>< EditIcon /></button>{" "}
-                <button  onClick={handleDelete}><DeleteOutlineOutlinedIcon /></button>
-            </div>
-        </div>
-    );
+
+
+  };
+
+
+
+
+
+
+    
+    const handleDelete = async () => {
+    try {
+      await fetch(`https://684235efe1347494c31c255d.mockapi.io/todos/${id}`, {
+        method: "DELETE",
+      });
+      fetchTodos()
+    } catch (error) {
+      console.error("Failed to delete task:", error);
+    }
+  };
+    
+  return ( 
+    
+    <div className="flex ">
+      <div className="flex justify-between items-center bg-white p-2 my-1 m  ml-20 text-lg first-letter:rounded shadow w-96">
+        <p 
+          className={item.complete ? "line-through text-gray-400 italic text-2xl" : "text-gray-900 text-2xl"}
+        >
+          {item.task}
+        </p>
+      </div>
+      <div className="flex items-center ">
+        <button className="bg-green-400 text-white p-2 my-1  text-2xl hover:bg-red-800" onClick={handleToggleComplete}>
+          {item.complete ? <CancelIcon />  : <CheckIcon /> }
+        </button>
+        <button className="bg-red-400 text-white p-2 my-1  text-lg  hover:bg-red-800" onClick={handleDelete}>
+          <DeleteOutlineOutlinedIcon />
+        </button>
+      </div>
+    </div>
+  );
 };
